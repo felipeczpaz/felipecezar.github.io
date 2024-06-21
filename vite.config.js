72a796342281d1481b802b339cacc5306769da3d
felipeczpaz <felipecezarpazzz@proton.me>
2024-06-21 19:25:33 -0300

    Added vite-plugin-html with minification enabled

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/felipecezar.github.io/',
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  base: "/felipecezar.github.io/",
+});
